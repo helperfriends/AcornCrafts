dd52662c888beeaecb2de3109a1a2603fa3600cc
helperfriends <129597169+helperfriends@users.noreply.github.com>
2023-04-02 14:08:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,46 +8,34 @@ const port = 9000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.post("/submit", (req, res) => {
-  const data = req.body;
+  try {
+    const data = req.body;
+    fs.appendFileSync("data.txt", JSON.stringify(data) + "\n");
+    console.log("Data saved successfully.");
+    res.sendFile(path.join(__dirname, "confirmation.html"));
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error saving data.");
+  }
+});
 
-  fs.appendFile("data.txt", JSON.stringify(data) + "\n", (err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error saving data.");
-    } else {
-      console.log("Data saved successfully.");
-      res.send("Data saved successfully.");
-    }
-  });
-  
-  // Save data to file
-  fs.appendFile("data.txt", JSON.stringify(req.body), (err) => {
-    if (err) throw err;
-    console.log("The file has been saved!");
-  });
-  
-  res.sendFile(path.join(__dirname, "confirmation.html"));
+app.get("/logs", (req, res) => {
+  try {
+    const data = fs.readFileSync("data.txt", "utf-8");
+    console.log("Data retrieved successfully.");
+    res.send(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error reading data.");
+  }
 });
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
-});
-
-app.get("/logs", (req, res) => {
-  fs.readFile("data.txt", "utf-8", (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error reading data.");
-    } else {
-      console.log("Data retrieved successfully.");
-      res.send(data);
-    }
-  });
 });
